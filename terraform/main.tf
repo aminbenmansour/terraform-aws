a8e9327959ea50c87158f381f66001ea0ad89bf7
Amine Ben Mansour <amin.benmansour.10@gmail.com>
2022-12-16 23:41:11 +0100

    launching an EC2 instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -74,3 +74,21 @@ resource "aws_key_pair" "mtc_auth" {
   key_name   = "mtckey"
   public_key = file("~/.ssh/mtckey.pub")
 }
+
+resource "aws_instance" "foo" {
+  ami           = data.aws_ami.server_ami.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "dev-node"
+  }
+
+  key_name = aws_key_pair.mtc_auth.id
+
+  vpc_security_group_ids = [aws_security_group.mtc_sg.id]
+  subnet_id = aws_subnet.mtc_public_subnet.id
+
+  root_block_device {
+    volume_size = 8
+  }
+}
